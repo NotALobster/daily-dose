10cec45983b820e51ae62be7d98a2046d73c755c
Perry <perryverse@gmail.com>
2024-02-03 20:44:03 -0800

    Text box fixed a bit :D

--- src/app/entry/page.js
+++ src/app/entry/page.js
@@ -3,15 +3,10 @@ import React from "react";
 
 function Entry() {
     return (
-        <div className="flex justify justify-center font-sans-arial text-6xl font-light px-3 mt-64">
-            <h2>Tell Me About Your Day:</h2>
-            <br/>
-            <div>
-                <form className="flex justify justify-center font-sans-arial text-3xl font-light px-2 mb-32">
-                    <label htmlFor="msg">Message:</label>
-                    <textarea id="msg" name="user_message"></textarea>
-                </form>
-            </div>
+        <div className="flex flex-col align-center font-sans-arial text-6xl font-light px-3 mt-64">
+            <h2>Tell Me About Your Day:</h2>            
+            
+            <textarea id="msg" name="user_message" className="w-[50vw]" style={{alignSelf: "center", border: "solid 1px black"}}></textarea>
         </div>
     );
 }
