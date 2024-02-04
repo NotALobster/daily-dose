331d649e4ae3f336aef28398881f204c0921f58a
Perry <perryverse@gmail.com>
2024-02-03 22:16:30 -0800

    home buttons

--- src/app/build/page.js
+++ src/app/build/page.js
@@ -1,9 +1,16 @@
 import React from "react"
 import Link from "next/link";
+import { CiPill } from "react-icons/ci";
 
 const Build = () => {
     return (
       <>
+      <div>
+        <CiPill className="flex justify-center items-center ml-6" size={100} />
+        <Link href="/home">
+          <div className="font-sans-arial text-l font-light px-3">D/D</div>
+        </Link>
+      </div>
         <div className="flex justify justify-center font-sans-arial text-5xl font-light px-3 mt-20">
             Add to today's capsule!
         </div>
