2fd1b2ebd645ce19fc927ad4246462daa82f4c26
Perry <perryverse@gmail.com>
2024-02-03 19:47:35 -0800

    kelly did dat

--- src/app/build/page.js
+++ src/app/build/page.js
@@ -7,6 +7,9 @@ const Build = () => {
         <div className="flex justify justify-center font-sans-arial text-5xl font-light px-3 mt-20">
             Add to today's capsule!
         </div>
+        <Link href="/entry">
+            <div className="flex justify justify-center font-sans-arial text-8xl font-light px-3">+</div>
+        </Link>
       </>
     );
   };
