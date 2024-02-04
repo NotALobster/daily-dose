8ed59c618e9795139f83c55947e86131975d15e2
Perry <perryverse@gmail.com>
2024-02-03 22:30:18 -0800

    boom home buttons

--- src/app/calendar/page.js
+++ src/app/calendar/page.js
@@ -1,21 +1,29 @@
 "use client";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Calendar from 'react-calendar';
-
+import { CiPill } from "react-icons/ci";
+import Link from "next/link";
 
 function App() {
   const [date, setDate] = useState(new Date());
 
   return (
-    <div className='app'>
-      <h1 className='text-center'>React Calendar</h1>
-      <div className='calendar-container'>
-        <Calendar onChange={setDate} value={date} />
+    <div>
+      <CiPill className="flex justify-center items-center ml-6" size={100} />
+      <Link href="/home">
+        <div className="font-sans-arial text-l font-light px-3">D/D</div>
+      </Link>
+
+      <div className='app'>
+        <h1 className='text-center'>React Calendar</h1>
+        <div className='calendar-container'>
+          <Calendar onChange={setDate} value={date} />
+        </div>
+        <p className='text-center'>
+          <span className='bold'>Selected Date:</span>{' '}
+          {date.toDateString()}
+        </p>
       </div>
-      <p className='text-center'>
-        <span className='bold'>Selected Date:</span>{' '}
-        {date.toDateString()}
-      </p>
     </div>
   );
 }
--- src/app/entry/page.js
+++ src/app/entry/page.js
@@ -4,18 +4,20 @@ import Link from "next/link";
 
 function Entry() {
     return (
-        <div>
-        <CiPill className="flex justify-center items-center ml-6" size={100} />
-        <Link href="/home">
-          <div className="font-sans-arial text-l font-light px-3">D/D</div>
-        </Link>
-        </div>
-        ,
-        <div className="flex flex-col align-center font-sans-arial text-6xl font-light px-3 mt-64">
-            <h2>Tell Me About Your Day:</h2>            
-            
-            <textarea id="msg" name="user_message" className="w-[50vw]" style={{alignSelf: "center", border: "solid 1px black"}}></textarea>
-        </div>
+        <>
+            <div>
+                <CiPill className="flex justify-center items-center ml-6" size={100} />
+                <Link href="/home">
+                    <div className="font-sans-arial text-l font-light px-3">D/D</div>
+                </Link>
+            </div>
+
+            <div className="flex flex-col align-center font-sans-arial text-6xl font-light px-3 mt-64">
+                <h2>Tell Me About Your Day:</h2>            
+                
+                <textarea id="msg" name="user_message" className="w-[50vw]" style={{ alignSelf: "center", border: "solid 1px black" }}></textarea>
+            </div>
+        </>
     );
 }
 
