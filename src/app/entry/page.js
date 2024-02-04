56ef2fecf69f45b0522853a2a346fc18a8c73970
Perry <perryverse@gmail.com>
2024-02-03 20:21:16 -0800

    entry page needs to be fixed

--- src/app/entry/page.js
+++ src/app/entry/page.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-const Entry = () => {
 
-        return (
+function Entry() {
+    return (
+        <div className="flex justify justify-center font-sans-arial text-6xl font-light px-3 mt-64">
+            <h2>Tell Me About Your Day:</h2>
+            <br/>
             <div>
-            <h2>Tell Me About Your Day</h2>
+                <form className="flex justify justify-center font-sans-arial text-3xl font-light px-2 mb-32">
+                    <label htmlFor="msg">Message:</label>
+                    <textarea id="msg" name="user_message"></textarea>
+                </form>
             </div>
-        )
+        </div>
+    );
 }
+
 export default Entry;
