da78a59e833740f76606f5187b33b76348f435dc
NotALobster <92136499+NotALobster@users.noreply.github.com>
2024-02-03 23:37:27 -0800

    improved entry page

--- src/app/entry/page.js
+++ src/app/entry/page.js
@@ -3,10 +3,11 @@ import React from "react";
 
 function Entry() {
     return (
-        <div className="flex flex-col align-center font-sans-arial text-6xl font-light px-3 mt-64">
-            <h2>Tell Me About Your Day:</h2>            
-            
-            <textarea id="msg" name="user_message" className="w-[50vw]" style={{alignSelf: "center", border: "solid 1px black"}}></textarea>
+        <div className="flex flex-col align-center font-sans-arial text-6xl font-light px-3 mt-64 space-y-16">
+            <h2 className="flex justify-center">Tell Me About Your Day:</h2>            
+            <div className="flex justify-center">
+                <textarea id="msg" name="user_message" className="w-[50vw]" style={{alignSelf: "center", border: "solid 1px black"}}></textarea>
+            </div>
         </div>
     );
 }
